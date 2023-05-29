349e2a9799a2aa86eda7bec8a42a4950a53ca70e
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-05-29 16:09:26 +0200

    boxers model

--- app/controllers/boxers_controller.rb
+++ app/controllers/boxers_controller.rb
@@ -1,2 +1,20 @@
 class BoxersController < ApplicationController
+  def index
+    @boxers = Boxer.all
+  end
+
+  def new
+    @boxer = Boxer.new
+  end
+
+  def create
+    @boxer = Boxer.new(boxers_params)
+    @boxer.user = User.find(params[:id])
+    @boxer.save
+    if @boxer.save
+      redirect_to boxer_path(@boxer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
