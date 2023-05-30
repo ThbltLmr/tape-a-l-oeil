12596b71d26e353592f69800feac7c1cbc64384f
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-05-30 12:21:47 +0200

    added user_id to boxers table, created booking model and bookings table, created seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_094115) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_100038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_094115) do
     t.integer "availability_radius"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_boxers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_094115) do
 
   add_foreign_key "bookings", "boxers"
   add_foreign_key "bookings", "users"
+  add_foreign_key "boxers", "users"
 end
