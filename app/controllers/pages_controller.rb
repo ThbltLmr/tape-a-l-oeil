e611567a92cd413e6c9bd399fb0e939946b43968
Thibault Lemery <lemery.thibault.pro@gmail.com>
2023-06-02 15:23:35 +0200

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,7 @@ class PagesController < ApplicationController
     @status_done = Booking.where(user: current_user, status: "done").count
     @status_validated = Booking.where(user: current_user, status: "validated").count
     @status_pending = Booking.where(user: current_user, status: "pending").count
+    @status_pending_boxer = Booking.where(boxer: current_user.boxer, status: "pending").count
+    @status_validated_boxer = Booking.where(boxer: current_user.boxer, status: "validated").count
   end
 end
